f2fa751700fbf5850e6a4d18fb6034c3a0f41dde
unknown <ivannikovmax04@gmail.com>
2023-10-05 21:54:50 +0300

    added currencies

--- lab2_3/src/main/java/com/example/lab2_3/controllers/Exchanges.java
+++ lab2_3/src/main/java/com/example/lab2_3/controllers/Exchanges.java
@@ -1,28 +1,28 @@
 package com.example.lab2_3.controllers;
 import com.example.lab2_3.dtos.CurrencyDTO;
 import com.example.lab2_3.services.impl.ExchangeServiceBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping(path = "/exchanges")
+@RequiredArgsConstructor
 public class Exchanges {
-    @Autowired
     private ExchangeServiceBean exchangeService;
 
-    @GetMapping("exchanges/latest")
+    @GetMapping("/latest")
     public String getAllExchanges(Model model){
         List<CurrencyDTO> exchanges = exchangeService.getAllTodayCurrencies();
         model.addAttribute("exchanges", exchanges);
         return "all_exchanges";
     }
 
-    @GetMapping("exchange/{source_curr}/{target_curr}/{dateFrom}/{dateTo}")
+    @GetMapping("exchanges/{source_curr}/{target_curr}/{dateFrom}/{dateTo}")
     public String getAllExchanges(Model model,
                                   @PathVariable("source_curr") final String source_curr,
                                   @PathVariable("target_curr") final String target_curr,
